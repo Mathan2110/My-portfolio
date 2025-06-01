2e114671ee4c13316c49d0871e70141fe0e3c990
Mathan2110 <maddy5410y@gmail.com>
2025-06-01 10:56:26 +0530

    changed link for resume to view

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
         <p className="home_para">Frontend developer from Ramanathapuram, currently in Chennai. A fresher passionate about web development with hands-on self-learning projects.</p>
         <div className="connect_resume_btns">
             <AnchorLink className='anchor' href='#contact' offset={50}><button className="connect">Connect With Me</button></AnchorLink>
-            <a className='resume' href="/Resume.pdf">My resume</a>
+            <a className='resume' href="/My-portfolio/Resume.pdf">My resume</a>
         </div>
     </div>
   )
