93923963f69a5ed342922729a8240bc93f0e080a
Mathan2110 <maddy5410y@gmail.com>
2025-06-23 11:04:43 +0530

    Changed Location

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,8 +6,8 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 const Home = () => {
   return (
     <div className='home_container' id='home'>
-        <h1 className='home_heading'><span>I'm Mathan Kumar,</span> Frontend Developer based in chennai.</h1>
-        <p className="home_para">Frontend developer from Ramanathapuram, currently in Chennai. A fresher passionate about web development with hands-on self-learning projects.</p>
+        <h1 className='home_heading'><span>I'm Mathan Kumar,</span> Frontend Developer based in Madurai.</h1>
+        <p className="home_para">Frontend developer from Ramanathapuram, currently in Madurai. A fresher passionate about web development with hands-on self-learning projects.</p>
         <div className="connect_resume_btns">
             <AnchorLink className='anchor' href='#contact' offset={50}><button className="connect">Connect With Me</button></AnchorLink>
             <a className='resume' href="/My-portfolio/Resume.pdf">My resume</a>
